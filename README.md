07fae85dd4a187efacdaefd471eead9750f020b2
Can Liu <can.liu@easystack.cn>
2018-02-28 10:55:08 +0800

    Parser Score Num

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # CVE
 
 Related pro:   https://github.com/CVEProject/cvelist
+
+Example:
+    ./update-cve.sh /home/lc/work/escore_source/cvelist 2017-12-25 8.0
+
+ 
--- update-cve.sh
+++ update-cve.sh
@@ -2,6 +2,8 @@
 
 REPO_DIR=$1
 LAST_TIME=$2
+SCORE_THRESHOLD=$3
+
 WORKDIR=`mktemp -d`
 CURRENT_DIR=`pwd`
 
@@ -16,12 +18,20 @@ COMMIT_ID=`git log --date=short | grep  -m 1 -B 3 'Date:   '$LAST_TIME| grep com
 
 git reset  -q  --soft $COMMIT_ID 
 
-#modified cve list
 git status| grep CVE-|awk '{print $NF}' |while read line ; 
 do  
 	[ `cat $line| grep 'CVSS 3.0 Base Score'| wc -l` -gt 0 ] && echo $line >> $WORKDIR/cve_score_list 
 done
 
+while read line;
+do
+    SCORE_NUM=`/usr/bin/python $CURRENT_DIR/get-severity.py $REPO_DIR/$line`
+    if [ `echo " $SCORE_NUM > $SCORE_THRESHOLD"|bc` -eq 1 ];then
+        #echo $SCORE_NUM
+        echo $line >> $WORKDIR/cve_list
+    fi
+done < $WORKDIR/cve_score_list
 
-cp $WORKDIR/cve_score_list $CURRENT_DIR
+
+cp $WORKDIR/cve_list $CURRENT_DIR
 rm $WORKDIR -fr
